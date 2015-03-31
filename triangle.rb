884c1d323ee9d24241e3588092ab3dcace15f951
lorisfo <loris-fo@outlook.com>
2015-03-31 14:39:25 +0100

    Answered exercises about triangles

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
-end
+[a, b, c].each { | side | raise TriangleError, "Error!" unless side > 0 }
+   raise TriangleError, "Error!" if ((a + b) <= c) | ((a + c) <= b) | ((b + c) <= a)
+   return :equilateral if (a == b) && (b == c)
+   return :isosceles if (a == b) || (b == c) || (a == c)
+   return :scalene
+ end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
